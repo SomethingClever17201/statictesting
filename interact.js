dc788240928822cb7d9033fee18b349beb3f7c4e
unknown <SomethingClever17201>
2022-03-10 19:50:30 +1100

    artistName

--- interact.js
+++ interact.js
@@ -58,6 +58,7 @@ function onLoad()
           element = document.createElement("p");
           let a = data.content + "";
         let puredata = a.substring(0,(top.clientWidth/7) * 12);
+        element.style.whiteSpace = "nowrap";
 
         element.style.padding = "5%";
            element.innerHTML = puredata;
