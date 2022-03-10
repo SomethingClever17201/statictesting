34765a60b1f3295320d38f196e05fe80eed7d1d1
unknown <SomethingClever17201>
2022-03-10 17:11:39 +1100

    artistName

--- interact.js
+++ interact.js
@@ -52,7 +52,6 @@ function onLoad()
 
           top = document.getElementById("content");
           element = document.createElement("p");
-          element.style.lineHeight = 1.2;
           let variable = Math.floor(top.style.height/1.3);
         let puredata = (data.content + "").substring(0,variable);
 
