6af6484177b85619245c1bf460188c2b2b4c8407
unknown <SomethingClever17201>
2022-03-10 19:54:36 +1100

    artistName

--- interact.js
+++ interact.js
@@ -58,7 +58,7 @@ function onLoad()
           element = document.createElement("p");
           let a = data.content + "";
         let puredata = a.substring(0,(top.clientWidth/7) * 12);
-        element.style.position = "relative";
+        element.style.position = "absolute";
         element.style.width = "100%";
 
         element.style.padding = "5%";
